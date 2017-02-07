ccf3e398a202fbfec87c26ec7df364442f8361e7
Matt Jones <gitcode@magisa.org>
2017-02-07 14:42:04 -0900

    Stub for data processing and upload.

---
+++ upload-packages.R
@@ -0,0 +1,19 @@
+# Upload data packages for Sevilleta community dynamics
+
+library(datapack)
+library(EML)
+
+process_dp1 <- function() {
+    # Create an EML document describing the data and R scripts
+
+    # Create identifiers for data and metadata objects
+
+    # Create a DataPackage to hold all of the objects
+
+    # Add each object to the DataPackage
+
+    # Add provenance information about the objects
+
+    # Upload package to the repository
+
+}
