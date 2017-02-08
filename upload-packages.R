82569a97c250e93927c1aa11176a4cdb36063c64
Corinna Gries <cgries@wisc.edu>
2017-02-07 19:13:09 -0600

    added more EML elements

--- upload-packages.R
+++ upload-packages.R
@@ -46,4 +46,66 @@ createEML <- function(){
     dc <- as.person("Scott Collins scollins@sevilleta.unm.edu")
     dataset_contact <- as(dc, "contact")
     dataset@contact <- new("ListOfcontact", c(dataset_contact))
+
+    #add keywords
+    keywordSet <- c(new("keywordSet",
+                        keyword = c("meteorology", "precipitation")))
+
+    dataset@keywordSet <- new("ListOfkeywordSet", c(keywordSet))
+
+    #intellectual Rights
+    dataset@intellectualRights <- as(set_TextType("intellectualRights.txt"), "intellectualRights")
+
+
+    #add methods
+    methods <- set_methods("methods.docx")
+
+    dataset@methods <- methods
+
+    #add coverage
+    begindate <- "1992-01-01"
+    enddate <- "2016-09-18"
+    geographicDescription <- "Five Points Meteorological Station (No. 49) is at the Southern edge of Mckenzie Flats - about 2.5 km west of the actual  Five-Points.  North of the road and just northeast of the intersection where another road takes off going north.  Transition area from creosote to the south into Grasslands to the north"
+    coverage <- set_coverage(begin = begindate, end = enddate,
+                             geographicDescription = geographicDescription,
+                             west = -106.729, east = -106.729,
+                             north = 34.335, south = 34.335)
+
+    dataset@coverage <- coverage
+
+
+    df <- read.csv("Met_all_excel.csv", header=TRUE, sep=",", quote="\"", as.is=TRUE)
+
+    #set up the attribute metadata csv file
+    rows <- ncol(df)
+
+    attributes <- data.frame(attributeName = character(rows),
+                             formatString = character(rows),
+                             unit = character(rows),
+                             numberType = character(rows),
+                             definition = character(rows),
+                             attributeDefinition = character(rows),
+                             columnClasses = character(rows),
+                             minimum = character(rows),
+                             maximum = character(rows),
+                             stringsAsFactors = FALSE)
+
+    #get some metadata from data frame
+    #add the column names to the template file
+    attributes$attributeName <- names(df)
+    #get the data types for each column
+    attributes$columnClasses <- sapply(df, class)
+    attributes$minimum <- sapply(df, min)
+    attributes$maximum <- sapply(df, max)
+    #set what R thinks is integer to numeric
+    attributes$columnClasses[attributes$columnClasses == "integer"] <- "numeric"
+    #write the prepared template to a csv file
+    write.csv(attributes, file = "met_all_csv_metadata.csv", row.names = FALSE)
+
+
+    #look at the standard units to get them right
+    standardUnits <- get_unitList()
+    View(standardUnits$units)
+
+
 }
