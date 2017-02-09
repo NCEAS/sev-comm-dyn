b510136b186bff0151afb906333873c6b48402f9
Corinna Gries <cgries@wisc.edu>
2017-02-09 08:50:31 -0600

    added code for dp3 and new add entity function, not tested yet

--- upload-packages.R
+++ upload-packages.R
@@ -21,10 +21,10 @@ process_dp1 <- function() {
     eml_file <- sprintf("%s/dp1-metadata.xml", dataDir)
     title <- "Sevilleta LTER Metstation number 49, precipition, daily raw and gap filled from 1992 - 2015"
     pubDate <- "2017"
-    abstract_file <- paste(dataDir,"abstractdp1.txt", sep = "/")
+    file_ext <- "dp1"
     keywords <- c("meteorology", "precipitation")
 
-    eml <- create_eml(title, pubDate, abstract_file, keywords)
+    eml <- create_eml(title, pubDate, file_ext, keywords, dataDir)
 
     # Create a DataObject to hold the script file and add it to the EML file
     file_name <- "Met_gap_fill.R"
@@ -86,10 +86,10 @@ process_dp2 <- function(){
     eml_file <- sprintf("%s/dp2-metadata.xml", dataDir)
     title <- "Sevilleta species list with life form designations"
     pubDate <- "2017"
-    abstract_file <- paste(dataDir,"abstractdp2.txt", sep = "/")
+    file_ext <- "dp2"
     keywords <- c("species names", "taxonomy", "life forms", "metabolism")
 
-    eml <- create_eml(title, pubDate, abstract_file, keywords)
+    eml <- create_eml(title, pubDate, file_ext, keywords, dataDir)
 
     # Document and add the first data file to the package
     file_name <- "sevilleta_species-list.csv"
@@ -102,8 +102,59 @@ process_dp2 <- function(){
 
 }
 
-create_eml <- function(title, pubDate, abstract_file, keywords){
+process_dp3 <- function(){
+
+    # Create a DataPackage to hold all of the objects
+    dp <- new("DataPackage")
+    dataDir <- getwd()
+    eml_file <- sprintf("%s/dp2-metadata.xml", dataDir)
+    title <- "Data analysis and graphs for publication"
+    pubDate <- "2017"
+    file_ext <- "dp3"
+    keywords <- c("species names", "taxonomy", "life forms", "metabolism")
+
+    eml <- create_eml(title, pubDate, file_ext, keywords, dataDir)
+
+    # Document and add the first file to the package
+    file_name <- "MSDS.jpg"
+    file_description <- "Odination of mice feeding guilds at two sites"
+    file_path <- sprintf("%s/%s", dataDir, file_name)
+    do1 <- new("DataObject", format="image/jpg", filename=file_path,
+               mediaType="image/jpg", suggestedFilename=file_name)
+    eml <- add_entity_eml(eml, file_name, file_description, file_path, do1@sysmeta@identifier, cn@endpoint)
+
+    # Document and add the second file to the package
+    file_name <- "Noble_BlackGramaCore.png"
+    file_description <- "Precipitation, biomass, NPP, and rodent abundance at black grama site"
+    file_path <- sprintf("%s/%s", dataDir, file_name)
+    do1 <- new("DataObject", format="image/png", filename=file_path,
+               mediaType="image/png", suggestedFilename=file_name)
+    eml <- add_entity_eml(eml, file_name, file_description, file_path, do1@sysmeta@identifier, cn@endpoint)
+
+    # Document and add the third file to the package
+    file_name <- "Noble_CreosoteCore.png"
+    file_description <- "Precipitation, biomass, NPP, and rodent abundance at creosote site"
+    file_path <- sprintf("%s/%s", dataDir, file_name)
+    do1 <- new("DataObject", format="image/png", filename=file_path,
+               mediaType="image/png", suggestedFilename=file_name)
+    eml <- add_entity_eml(eml, file_name, file_description, file_path, do1@sysmeta@identifier, cn@endpoint)
+
+    # Create a DataObject to hold the script file and add it to the EML file
+    file_name <- "veg_analysis.R"
+    file_description <- "R script that manipulates input data, runs analysis and generates graphs"
+    file_path <- sprintf("%s/%s", dataDir, file_name)
+    progObj <- new("DataObject", format="application/R", filename=file_path,
+                   mediaType="text/x-rsrc", suggestedFilename=file_name)
+    eml <- add_entity_eml(eml, file_name, file_description, file_path, progObj@sysmeta@identifier, cn@endpoint)
+
+
+}
+
+
+create_eml <- function(title, pubDate, file_ext, keywords, dataDir){
     eml <- new("eml")
+
+    abstract_file <- paste(dataDir, paste("abstract", file_ext, ".txt", sep = ""), sep = "/")
 
     abstract <- as(set_TextType(abstract_file), "abstract")
 
@@ -286,6 +337,102 @@ add_entity_eml <- function(eml, entity_name, entity_description, file_path, iden
 
         return(eml)
     }
+
+
 }
 
+add_entity_eml_new <- function(eml, entity_name, entity_description, file_path, identifier, resolve_uri) {
+
+    if (stringr::str_detect(file_path, '.*.csv$')){
+        file_name <- basename(file_path)
+        path_only <- dirname(file_path)
+
+        #define custom units
+        unitTypefile_name <- paste(file_name, "unitType.csv", sep = "")
+        if(file.exists(paste(path_only, unitTypefile_name, sep = "/"))){
+            unitType <- read.csv(paste(path_only, unitTypefile_name, sep = "/"), header = TRUE, sep = ",", quote = "\"", as.is = TRUE, na.strings = "")
+            custom_unitsfile_name <- paste(file_name, "custom_units.csv", sep = "")
+            custom_units <- read.csv(paste(path_only, custom_unitsfile_name, sep = "/"), header = TRUE, sep = ",", quote = "\"", as.is = TRUE, na.strings = "")
+            unitsList <- set_unitList(custom_units, unitType)
+            additionalMetadata <- as(unitsList, "additionalMetadata")
+            eml@additionalMetadata <- c(additionalMetadata)
+        }
+
+        #read the attributes file back in with all new entries
+        attrmetafile_name <- paste(file_name,"meta.csv", sep = "")
+        attributes <- read.csv(paste(path_only, attrmetafile_name, sep = "/"), header = TRUE, sep = ",", quote = "\"", as.is = TRUE, na.strings = "")
+
+        factor_meta <- paste(path_only, paste(file_name, "factor.csv", sep = ""), sep = "/")
+
+        if(file.exists(factor_meta)){
+            factors <- read.csv(factor_meta, header = TRUE, sep = ",", quote = "\"", as.is = TRUE)
+        }
+
+        # get the column classes into a vector as required by the set_attribute function
+        col_classes <- attributes[,"columnClasses"]
+
+        #take out that column again
+        attributes$columnClasses <- NULL
+
+        #with the attributes data frames in place we can create the attributeList element - no factors need to be defined for this dataset
+        attributeList <- set_attributes(attributes, col_classes = col_classes)
+
+        #physical parameter for standard Microsoft csv file
+        resolve_url <- paste(resolve_uri, identifier, sep="/")
+
+        physical <- set_physical(file_name,
+                                 numHeaderLines = "1",
+                                 recordDelimiter = "\\r\\n",
+                                 url = resolve_url)
+
+
+        #pull to gether information for the dataTable
+
+        dataTable <- new("dataTable",
+                         entityName = entity_name,
+                         entityDescription = entity_description,
+                         physical = physical,
+                         attributeList = attributeList)
+
+        current_dataTables <- eml@dataset@dataTable
+        current_dataTables[[length(current_dataTables)+1]] <- dataTable
+        eml@dataset@dataTable <- current_dataTables
+
+
+        return(eml)
+
+    }else{
+
+        if (stringr::str_detect(file_path, '.*.R$')){
+            entityType <- "text/x-rsrc"
+            formatName <- "application/R"
+        }else if(stringr::str_detect(file_path, '.*.png$')){
+            entityType <- "image/png"
+            formatName <- "image/png"
+        }else if(stringr::str_detect(file_path, '.*.jpg$')){
+            entityType <- "image/jpg"
+            formatName <- "image/jpg"
+        }
+
+
+        other_entity <- new("otherEntity")
+        other_entity@entityName <- entity_name
+        other_entity@entityDescription <- entity_description
+        other_entity@entityType <- entityType
+        resolve_url <- paste(resolve_uri, identifier, sep="/")
+        online <- new("online")
+        online@url <- new("url", resolve_url)
+        dist <- new("distribution")
+        dist@online <- online
+        phys <- new("physical")
+        phys@objectName <- basename(file_path)
+        phys@dataFormat@externallyDefinedFormat@formatName <- formatName
+        phys@distribution <- c(dist)
+        other_entity@physical <- c(phys)
+        current_other_entities <- eml@dataset@otherEntity
+        current_other_entities[[length(current_other_entities)+1]] <- other_entity
+        eml@dataset@otherEntity <- current_other_entities
+        return(eml)
+    }
+}
 
